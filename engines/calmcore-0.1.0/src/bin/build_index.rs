3b4e614154497dea6079d33a56e9611f1b5ca107
shiyuan1 <shiyuan1@ybm100.com>
2025-04-09 20:12:19 +0800

    basic structure for benchmark

---
+++ engines/calmcore-0.1.0/src/bin/build_index.rs
@@ -0,0 +1,89 @@
+use std::env;
+use std::io::BufRead;
+use std::time::Instant;
+
+use calmcore;
+use calmcore::{Action, ActionType, CalmCore, Config};
+use calmcore::util::CoreResult;
+use proto::core::field::{FulltextOption, TermOption, Type};
+use proto::core::field::fulltext_option::Tokenizer;
+use proto::core::field::Option::Fulltext;
+use proto::core::field::Option::Term;
+use proto::core::Schema;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    main_inner("calmcore-bench", &args[1]).unwrap();
+}
+
+fn main_inner(schema_name: &str, data_path: &str) -> CoreResult<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let schema = make_schema(schema_name);
+    let core = CalmCore::new_with_conf(Config {
+        data_path: data_path.to_string(),
+        segment_max_size: 10_000,
+        flush_interval_secs: 300,
+    })?;
+    let space = core.create_engine(schema)?;
+
+    println!("Starting data insertion...");
+    let start = Instant::now();
+
+    let batch_size = 1000;
+    let mut total = 0;
+    let mut actions = Vec::with_capacity(batch_size);
+
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        if line.trim().is_empty() {
+            continue;
+        }
+        total += 1;
+
+        let value = serde_json::from_str::<serde_json::Value>(&line)?;
+        let name = value.get("id").unwrap().as_str().unwrap();
+        actions.push(Action::new(ActionType::Append, name, line.as_bytes()));
+
+        if total % 1000 == 0 {
+            space.mutate(actions, None)?;
+            actions = Vec::with_capacity(batch_size);
+        }
+
+        if total % 100_000 == 0 {
+            println!("{}", total);
+
+            if space.segment_readers().iter().filter(|r| r.is_hot()).count() > 3 {
+                println!("Too many hot segments, waiting for compaction...");
+                std::thread::sleep(std::time::Duration::from_secs(5));
+            }
+        }
+    }
+
+    if !actions.is_empty() {
+        space.mutate(actions, None)?;
+    }
+
+    space.persist()?;
+
+    println!("Data insertion finished in {:?}", start.elapsed());
+
+    Ok(())
+}
+
+fn make_schema(schema_name: &str) -> Schema {
+    calmcore::easy_schema(
+        schema_name,
+        vec![
+            ("id".to_string(), Type::String, Some(Term(TermOption { no_index: true, no_store: false }))),
+            ("text".to_string(), Type::Text, Some(Fulltext(FulltextOption {
+                tokenizer: Tokenizer::Standard as i32,
+                filters: Vec::new(),
+                stopwords: None,
+                synonyms: None,
+                no_store: true,
+            }))),
+        ],
+    )
+}
---
+++ engines/calmcore-0.1.0/src/bin/do_query.rs
@@ -0,0 +1,99 @@
+use std::env;
+use std::io::BufRead;
+
+use calmcore::CalmCore;
+use calmcore::util::CoreResult;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    main_inner("calmcore-bench", &args[1]).unwrap()
+}
+
+fn main_inner(schema_name: &str, data_path: &str) -> CoreResult<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let sql = format!("select * from {} where ", schema_name);
+
+    let core = CalmCore::new(data_path)?;
+    let engine = core.load_engine(schema_name)?;
+
+    let stdin = std::io::stdin();
+    for line_res in stdin.lock().lines() {
+        let line = line_res?;
+        let fields: Vec<&str> = line.split("\t").collect();
+        assert_eq!(fields.len(), 2, "Expected a line in the format <COMMAND> query.");
+
+        let command = fields[0];
+        let query = parse_query(fields[1]);
+        let count;
+        match command {
+            "COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 0").as_str())?.total_hits;
+            }
+            "TOP_10" => {
+                let _top_k = engine.sql(format!("{sql} {query} limit 10").as_str())?;
+                count = 1;
+            }
+            "TOP_100" => {
+                let _top_k = engine.sql(format!("{sql} {query} limit 100").as_str())?;
+                count = 1;
+            }
+            "TOP_1000" => {
+                let _top_k = engine.sql(format!("{sql} {query} limit 1000").as_str())?;
+                count = 1;
+            }
+            "TOP_1_COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 1").as_str())?.total_hits;
+            }
+            "TOP_5_COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 5").as_str())?.total_hits;
+            }
+            "TOP_10_COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 10").as_str())?.total_hits;
+            }
+            "TOP_100_COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 100").as_str())?.total_hits;
+            }
+            "TOP_1000_COUNT" => {
+                count = engine.sql(format!("{sql} {query} limit 1000").as_str())?.total_hits;
+            }
+            _ => {
+                println!("UNSUPPORTED");
+                continue;
+            }
+        }
+        println!("{}", count);
+    }
+
+    Ok(())
+}
+
+fn parse_query(query: &str) -> String {
+    if query.contains('"') {
+        if query.contains('+') {
+            // tags:two-phase-critic: +"the who" +uk
+            query.split('+')
+                .filter(|e| !e.trim().is_empty())
+                .map(|e| {
+                    let s = e.trim();
+                    if s.contains('"') {
+                        format!("text=phrase('{}',slop=0)", s.replace('"', ""))
+                    } else {
+                        format!("text='{}'", s)
+                    }
+                })
+                .collect::<Vec<_>>().join(" and ")
+        } else {
+            // tags:phrase
+            format!("text=phrase('{}',slop=0)", query.replace('"', ""))
+        }
+    } else {
+        if query.contains('+') {
+            // tags:intersection
+            format!("text=text('{}',operator='and')", query.replace('+', ""))
+        } else {
+            // tags:union,term
+            format!("text=text('{}',operator='or')", query)
+        }
+    }
+}
